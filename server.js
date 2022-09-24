7e402d18d5797d94959d734323138e82c811906b
ArtyomAmirdzhanyans <tyoma_manager@mail.ru>
2022-09-24 16:09:53 +0300

    Server redirected

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static("build"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(5000, () => {
+  console.log("Server started")
+});
