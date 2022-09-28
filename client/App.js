174edf52c607097d83178b78374eb057982c5a19
ArtyomAmirdzhanyans <tyoma_manager@mail.ru>
2022-09-28 23:55:57 +0300

    Setting up the client side, writing API scripts

--- client/App.js
+++ client/App.js
@@ -1,7 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div>
-      Hello World
+    <div className="App">
+      <ToastContainer position="center" />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,10 +1,80 @@
+/* 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'tyoma1991',
+  database: 'user_management'
+});
 
 app.use(express.static("build"));
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(5000, () => {
-  console.log("Server started")
+app.get("/", (req, res) => {
+  res.send("Hello Express")
+  console.log("Hello express")
+})
+
+app.get("/api/get", (req, res) => {
+  const sqlGet = "SELECT * FROM users";
+  db.query(sqlGet, (error, result) => {
+    if (error) {
+      console.log(error);
+    } res.send(result);
+  });
 });
+
+app.post("/api/post", (req, res) => {
+  const { name, email, contact } = req.body;
+  const sqlInsert = "INSERT INTO users (name, email, contact) VALUES (?, ?, ?)"
+  db.query(sqlInsert, [name, email, contact], (error, result) => {
+    if (error) {
+      console.log(error);
+    };
+  });
+});
+
+app.put("/api/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, email, contact } = req.body;
+  const sqlUpdate = "UPDATE users SET name = ?, email = ?, contact = ? WHERE id = ?";
+
+  db.query(sqlUpdate, [name, email, contact, id], (error, result) => {
+    if (error) {
+      console.log(error);
+    } res.send(result);
+  });
+});
+
+app.delete("/api/remove/:id", (req, res) => {
+  const { id } = req.params;
+  const sqlRemove = "DELETE FROM users WHERE id = ?"
+  db.query(sqlRemove, id, (error, result) => {
+    if (error) {
+      console.log(error);
+    };
+  });
+});
+
+app.get("/api/get/:id", (req, res) => {
+  const { id } = req.params;
+  const sqlGet = "SELECT * FROM users WHERE id = ?";
+  db.query(sqlGet, id, (error, result) => {
+    if (error) {
+      console.log(error);
+    };
+    res.send(result);
+  });
+}); 
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000")
+});
+*/
